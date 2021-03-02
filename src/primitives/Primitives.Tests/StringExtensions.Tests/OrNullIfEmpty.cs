301769896d9e56469283a710d844d29d539f5d72
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-02 21:34:10 +0300

    Add OrNullIfEmpty/WhiteSpace tests

---
+++ src/primitives/Primitives.Tests/StringExtensions.Tests/OrNullIfEmpty.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+using NUnit.Framework;
+using System;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Primitives.Tests
+{
+    partial class StringExtensionsTests
+    {
+        [Test]
+        public void OrNullIfEmpty_SourceIsEmpty_ExpectNull()
+        {
+            string? source = string.Empty;
+
+            var actual = source.OrNullIfEmpty();
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase(WhiteSpaceString)]
+        [TestCase(TabString)]
+        [TestCase(SomeString)]
+        public void OrNullIfEmpty_SourceIsNotEmpty_ExpectSourceValue(
+            string? source)
+        {
+            var actual = source.OrNullIfEmpty();
+            Assert.AreSame(source, actual);
+        }
+    }
+}
