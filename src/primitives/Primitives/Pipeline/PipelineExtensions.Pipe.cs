46050d8a34158c1e73e319e873882d812d18188a
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-14 16:29:24 +0300

    Refactor: Split Pipe and PipeSelf files

---
+++ src/primitives/Primitives/Pipeline/PipelineExtensions.Pipe.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace System
+{
+    partial class PipelineExtensions
+    {
+        public static TResult Pipe<T, TResult>(this T value, Func<T, TResult> pipe)
+        {
+            _ = pipe ?? throw new ArgumentNullException(nameof(pipe));
+            
+            return pipe.Invoke(value);
+        }
+    }
+}
--- src/primitives/Primitives/Pipeline/PipelineExtensions.PipeSelf.cs
+++ src/primitives/Primitives/Pipeline/PipelineExtensions.PipeSelf.cs
@@ -1,26 +1,15 @@
 ﻿#nullable enable
 
-namespace System
-{
-    partial class PipelineExtensions
-    {
-        public static TResult Pipe<T, TResult>(this T value, Func<T, TResult> pipe)
-        {
-            _ = pipe ?? throw new ArgumentNullException(nameof(pipe));
-            return pipe.Invoke(value);
-        }
-    }
-}
+using System;
 
 namespace PrimeFuncPack
 {
-    using System;
-
     partial class PipelineExtensions
     {
         public static TResult PipeSelf<T, TResult>(this T value, Func<T, TResult> pipe)
         {
             _ = pipe ?? throw new ArgumentNullException(nameof(pipe));
+            
             return pipe.Invoke(value);
         }
     }
