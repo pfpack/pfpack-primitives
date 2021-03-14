a04fdaf825d81f78c797e54a00b3453c3f4f4310
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-14 16:58:09 +0300

    Add Forced Inlining for Pipeline Extensions

--- src/primitives/Primitives/Pipeline/PipelineExtensions.Pipe.cs
+++ src/primitives/Primitives/Pipeline/PipelineExtensions.Pipe.cs
@@ -1,9 +1,12 @@
 ﻿#nullable enable
 
+using System.Runtime.CompilerServices;
+
 namespace System
 {
     partial class PipelineExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TResult Pipe<T, TResult>(this T value, Func<T, TResult> pipe)
         {
             _ = pipe ?? throw new ArgumentNullException(nameof(pipe));
--- src/primitives/Primitives/Pipeline/PipelineExtensions.PipeSelf.cs
+++ src/primitives/Primitives/Pipeline/PipelineExtensions.PipeSelf.cs
@@ -1,11 +1,13 @@
 ﻿#nullable enable
 
 using System;
+using System.Runtime.CompilerServices;
 
 namespace PrimeFuncPack
 {
     partial class PipelineExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TResult PipeSelf<T, TResult>(this T value, Func<T, TResult> pipe)
         {
             _ = pipe ?? throw new ArgumentNullException(nameof(pipe));
