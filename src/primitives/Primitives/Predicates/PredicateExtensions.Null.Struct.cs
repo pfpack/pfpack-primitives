e518707444c4edfa7fbc8e027c26e3b61bd4e24a
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-16 17:18:22 +0300

    Arrange Null predicate files

--- src/primitives/Primitives/Predicates/PredicateExtensions.Null.Struct.cs
+++ src/primitives/Primitives/Predicates/PredicateExtensions.Null.Struct.cs
@@ -8,19 +8,9 @@ namespace System
     partial class PredicateExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsNotNull<T>([NotNullWhen(true)][MaybeNullWhen(false)] this T? value)
-            =>
-            value is not null;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsNotNull<T>([NotNullWhen(true)] this T? value) where T : struct
             =>
             value is not null;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsNull<T>([NotNullWhen(false)][MaybeNullWhen(true)] this T? value)
-            =>
-            value is null;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsNull<T>([NotNullWhen(false)] this T? value) where T : struct
---
+++ src/primitives/Primitives/Predicates/Predicates.Null.Common.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace System
+{
+    partial class Predicates
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNotNull<T>([NotNullWhen(true)][MaybeNullWhen(false)] T? value)
+            =>
+            value is not null;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNull<T>([NotNullWhen(false)][MaybeNullWhen(true)] T? value)
+            =>
+            value is null;
+    }
+}
