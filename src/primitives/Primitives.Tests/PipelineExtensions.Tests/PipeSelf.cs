fde3f47262e7bc59e526ee178620e8565ef6073c
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-03-28 16:40:22 +0300

    Comment the obsolete extension test

--- src/primitives/Primitives.Tests/PipelineExtensions.Tests/PipeSelf.cs
+++ src/primitives/Primitives.Tests/PipelineExtensions.Tests/PipeSelf.cs
@@ -7,15 +7,15 @@ namespace PrimeFuncPack.Primitives.Tests
 {
     partial class PipelineExtensions
     {
-        [Test]
-        [TestCaseSource(nameof(ValueTestSource))]
-        public void PipeSelf_ExpectMappedSourceValue(
-            object? sourceValue)
-        {
-            var actual = sourceValue.PipeSelf(value => (value, PlusFifteen));
-            var expected = (sourceValue, PlusFifteen);
-            Assert.AreSame(expected.sourceValue, sourceValue);
-            Assert.AreEqual(expected, actual);
-        }
+        // [Test]
+        // [TestCaseSource(nameof(ValueTestSource))]
+        // public void PipeSelf_ExpectMappedSourceValue(
+        //     object? sourceValue)
+        // {
+        //     var actual = sourceValue.PipeSelf(value => (value, PlusFifteen));
+        //     var expected = (sourceValue, PlusFifteen);
+        //     Assert.AreSame(expected.sourceValue, sourceValue);
+        //     Assert.AreEqual(expected, actual);
+        // }
     }
 }
