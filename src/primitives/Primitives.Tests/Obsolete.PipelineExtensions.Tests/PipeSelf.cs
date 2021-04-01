cee8b900048001a25cc157997291c7db08833901
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-04-01 20:13:07 +0300

    Implement obsolete PipeSelf test

--- src/primitives/Primitives.Tests/Obsolete.PipelineExtensions.Tests/PipeSelf.cs
+++ src/primitives/Primitives.Tests/Obsolete.PipelineExtensions.Tests/PipeSelf.cs
@@ -1,21 +1,49 @@
 ﻿#nullable enable
 
 using NUnit.Framework;
-using static PrimeFuncPack.UnitTest.TestData;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
 
 namespace PrimeFuncPack.Primitives.Tests
 {
     partial class ObsoletePipelineExtensionsTests
     {
-        // [Test]
-        // [TestCaseSource(nameof(ValueTestSource))]
-        // public void PipeSelf_ExpectMappedSourceValue(
-        //     object? sourceValue)
-        // {
-        //     var actual = sourceValue.PipeSelf(value => (value, PlusFifteen));
-        //     var expected = (sourceValue, PlusFifteen);
-        //     Assert.AreSame(expected.sourceValue, sourceValue);
-        //     Assert.AreEqual(expected, actual);
-        // }
+        [Obsolete]
+        [Test]
+        public void PipeSelf_ExpectIsObsolete()
+        {
+            const string expectedObsoleteMessage
+                = "This extension method is obsolete. Call System.PipelineExtensions.Pipe instead.";
+
+            IReadOnlyCollection<MethodInfo> methods =
+                typeof(PipelineExtensions) // PrimeFuncPack.PipelineExtensions
+                .GetMethods(
+                    BindingFlags.Public | BindingFlags.Static)
+                .Where(
+                    method => method.Name == nameof(PipelineExtensions.PipeSelf)) // PrimeFuncPack.PipelineExtensions.PipeSelf
+                .ToArray();
+
+            Assert.AreEqual(1, methods.Count);
+
+            Assert.True(
+                methods.All(
+                    method => method.CustomAttributes.Any(
+                        attr
+                        =>
+                        attr.AttributeType == typeof(ObsoleteAttribute) &&
+                        attr.ConstructorArguments.Count == 2 &&
+                        attr.ConstructorArguments[0].Value is expectedObsoleteMessage &&
+                        attr.ConstructorArguments[1].Value is true)));
+
+            Assert.True(
+                methods.All(
+                    method => method.CustomAttributes.Any(
+                        attr
+                        =>
+                        attr.AttributeType == typeof(DoesNotReturnAttribute))));
+        }
     }
 }
