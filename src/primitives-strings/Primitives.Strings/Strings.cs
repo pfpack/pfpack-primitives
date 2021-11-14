3830a11ddab7bfc5437ef6398eecf2f8fb401d7b
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-11-14 22:38:51 +0300

    Deprecate ToStringOrEmpty

--- src/primitives-strings/Primitives.Strings/Strings.cs
+++ src/primitives-strings/Primitives.Strings/Strings.cs
@@ -28,6 +28,8 @@ public static class Strings
             ? null
             : value;
 
+    // TODO: Consider to delete the method in v3.0
+    [Obsolete(InternalObsoleteMessages.ToStringOrEmpty, error: InternalObsoleteErrors.ToStringOrEmpty)]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ToStringOrEmpty<T>(T? value)
         =>
